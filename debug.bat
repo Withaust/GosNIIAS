332c031be963e419524eaef39596069989e02714
Withaust <withaust@gmail.com>
2025-06-25 03:18:40 +0300

    Added readme with a picture & offset .bat timings

--- debug.bat
+++ debug.bat
@@ -1,3 +1,3 @@
 start x64/Debug/GosNIIAS.exe server 127.0.0.1 12345
-sleep 1
+sleep 100
 start x64/Debug/GosNIIAS.exe client 127.0.0.1 12346 127.0.0.1 12345
--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 start x64/Release/GosNIIAS.exe server 127.0.0.1 12345
-sleep 1
+sleep 100
 start x64/Release/GosNIIAS.exe client 127.0.0.1 12346 127.0.0.1 12345
